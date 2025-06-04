49ca8a58053a3b169af27a0fff0334b176e088c7
React X BORODINO123 <mshustov131213@gmail.com>
2025-06-04 14:03:04 +0300

    Update install-arch-chapter-two.sh

--- install-arch-chapter-two.sh
+++ install-arch-chapter-two.sh
@@ -1,29 +1,23 @@
-pacman -Sy archlinux-keyring && \
-loadkeys ru && \
-timedatectl set-ntp true && \
-(echo "g;n;1;;+1G;t;1;n;2;;;w;" | fdisk /dev/sda) && \
+parted -s /dev/sda mklabel gpt && \
+parted -s /dev/sda mkpart primary fat32 1MiB 513MiB && \
+parted -s /dev/sda set 1 esp on && \
+parted -s /dev/sda mkpart primary ext4 513MiB 100% && \
 mkfs.fat -F32 /dev/sda1 && \
 mkfs.ext4 /dev/sda2 && \
 mount /dev/sda2 /mnt && \
-mkdir /mnt/boot && \
-mount /dev/sda1 /mnt/boot && \
-pacstrap /mnt base linux linux-firmware nano grub && \
+mkdir -p /mnt/boot/efi && \
+mount /dev/sda1 /mnt/boot/efi && \
+pacstrap /mnt base linux linux-firmware nano grub efibootmgr networkmanager && \
 genfstab -U /mnt >> /mnt/etc/fstab && \
-arch-chroot /mnt bash -c "ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime && \
+arch-chroot /mnt /bin/bash -c "ln -sf /usr/share/zoneinfo/Europe/Moscow /etc/localtime && \
 hwclock --systohc && \
-echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen && \
+echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen && \
 locale-gen && \
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf && \
-echo 'KEYMAP=ru' > /etc/vconsole.conf && \
-echo 'arch' > /etc/hostname && \
-echo '127.0.0.1 localhost' >> /etc/hosts && \
-echo '::1 localhost' >> /etc/hosts && \
-echo '127.0.1.1 arch.localdomain arch' >> /etc/hosts && \
-useradd -mG wheel -s /bin/bash miksa
-passwd miksa && \
-grub-install /dev/sda && \
+echo 'arch-sigma' > /etc/hostname && \
+passwd && \
+grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB && \
 grub-mkconfig -o /boot/grub/grub.cfg && \
-systemctl enable dhcpcd && \
 exit" && \
 umount -R /mnt && \
-reboot
+echo -e "\n\033[32m[+] Arch Linux установлен! Перезагружайся командой: reboot\033[0m"
